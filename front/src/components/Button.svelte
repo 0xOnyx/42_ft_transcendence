1d3714bf3ecfbfeb5243574ce5b830bf59979358
Daniel Thalmann <daniel@thalmann.li>
2023-04-05 23:49:56 +0200

    User profil

--- front/src/components/Button.svelte
+++ front/src/components/Button.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	export let name : string = 'Button name';
 	export let url : string = '';
+    export let color : string = 'bg-color2';
+    export let width : string = '';
 </script>
 
-<a class="bg-color2 px-8 py-2 rounded-md inline-block text-black" href={url}>{name}</a>
+<a class="{color} px-8 py-2 {width} rounded-md inline-block text-black" href={url}>{name}</a>
