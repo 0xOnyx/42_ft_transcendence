baa92bf325dcbbe4d185d639eefa270569b92a5f
Daniel Thalmann <daniel@thalmann.li>
2023-04-15 22:20:49 +0200

    add types

---
+++ front/src/types/user.ts
@@ -0,0 +1,20 @@
+export type User =
+{
+    id: number,
+    name?: string,
+    email?: string,
+    first_name?: string,
+    last_name?: string,
+    image_url?: string,
+    oauth_42_login?: string,
+    oauth_42_id?: number,
+    last_login?: Date,
+    online_status?: Status,
+}
+
+
+export enum Status {
+    ONLINE = 'ONLINE',
+    OFFLINE = 'OFFLINE',
+    HIDDEN = 'HIDDEN'
+};
