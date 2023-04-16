61f3a734daf0f8201d917a2206090b33b7e9db50
Daniel Thalmann <daniel@thalmann.li>
2023-04-16 10:48:00 +0200

    add icon

--- front/src/components/Button.svelte
+++ front/src/components/Button.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	export let name : string = 'Button name';
 	export let url : string = '';
-    export let color : string = 'bg-color2';
+    export let color : string = 'bg-color2 border-2 border-color2';
     export let width : string = '';
 </script>
 
-<a class="{color} px-8 py-2 {width} rounded-md inline-block text-black" href={url}>{name}</a>
+<a class="{color} px-8 py-1 {width} rounded-md inline-block text-black" href={url}>{name}</a>
