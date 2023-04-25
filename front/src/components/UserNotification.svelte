bdfcfb638b3b5a98e745784b29de84a58e97dbab
Guillaume <Guillaume@aubert.ws>
2023-04-25 12:46:19 +0000

    Adding notification dot in UserNotification

--- front/src/components/UserNotification.svelte
+++ front/src/components/UserNotification.svelte
@@ -7,8 +7,9 @@
 
 <div class="flex bg-color5 rounded-xl items-center">
 
-    <div class="">
-
+    <div class="relative">
+		<span class="sr-only">Notifications</span>
+		<div class="absolute inline-flex items-center justify-center min-w-60 h-6 text-xs px-1 font-bold text-white bg-red-500 border-2 border-white rounded-full -top-1 -left-1 dark:border-gray-900">0</div>
         <Icon icon="bell" css="inline m-3 h-8 stroke-2 stroke-white fill-none"></Icon>
 
     </div>
