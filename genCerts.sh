8e83e941e529c5487ec2b84bb66cba06625ff200
Guillaume <Guillaume@aubert.ws>
2023-04-28 09:26:59 +0000

    Not working yet :(

---
+++ genCerts.sh
@@ -0,0 +1,1 @@
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out ./docker/config/nginx/localhost.crt -keyout ./docker/config/nginx/localhost.key -subj "/C=CH/ST=Lausanne/L=Lausanne/O=42/OU=gaubert/CN=localhost.ch"
