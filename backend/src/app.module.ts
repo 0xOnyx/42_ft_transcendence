ead2c5e38a401760c2459b60ba920c6063eeb77c
Daniel Thalmann <daniel@thalmann.li>
2023-04-29 12:11:07 +0200

    add module game to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,10 +7,11 @@ import { UserModule } from './user/user.module';
 import { EventWsModule } from './eventWs/eventWsModule';
 import { MessageModule } from './message/message.module';
 import {ConfigModule} from "@nestjs/config";
+import { GameModule } from './game/game.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule, UserModule, EventWsModule, MessageModule],
+  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule, UserModule, EventWsModule, MessageModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
