c0a6efb79d92b717af74df9324e6db95d225d021
Daniel Thalmann <daniel@thalmann.li>
2023-04-30 09:52:06 +0200

    prepare to deactive ssr

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
     import "../app.css";
-	export const ssr = false;
 </script>
 
 <svelte:head>
@@ -12,7 +11,7 @@
 	<div class="absolute -bottom-36 -left-36 w-72 h-72 bg-process-green rounded-full filter mix-blend-multiply blur-3xl animate-idle"></div>
 	<div class="absolute top-1/2 left-1/2 w-72 h-72 bg-gradient-to-bl from-process-green to-thread-blue rounded-full filter mix-blend-overlay blur-3xl animate-idle animation-delay-4000"></div>
 	<div class="absolute -top-36 -right-36 w-72 h-72 bg-thread-blue rounded-full filter mix-blend-multiply blur-3xl animate-idle animation-delay-2000"></div>
-	
+
 	<div class="z-10 relative h-screen">
 		<slot />
 	</div>
