6ce7c45c1234e80c249bcc85ab3579dee9ff9dce
Daniel Thalmann <daniel@thalmann.li>
2023-04-30 09:49:08 +0200

    create a component checkbox

---
+++ front/src/components/Checkbox.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    // https://stackoverflow.com/questions/74974066/visible-non-interactive-elements-with-an-onclick-event-must-be-accompanied-by
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+	export let checked : boolean = false;
+    export let disable : boolean = false;
+
+    function toggle()
+    {
+        if(!disable) {
+            checked = !checked;
+            onChange();
+        }
+    }
+    function keyToggle(event : KeyboardEvent)
+    {
+        if(!disable) {
+            if(event.key === " "){
+                checked = !checked;
+                onChange();
+            }
+        }
+    }
+    function onChange()
+    {
+		dispatch('change', {
+			"checked": checked
+		});
+    }
+</script>
+
+<div role="checkbox" aria-checked={checked} tabindex="0" class="w-5 h-5 bg-color5 inline-block rounded-md p-1" on:click={toggle} on:keyup={keyToggle}>
+    {#if checked}
+    <div class="bg-color3 h-full w-full rounded-sm"></div>
+    {/if}
+</div>
