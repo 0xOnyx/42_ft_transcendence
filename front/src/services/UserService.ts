24e2d8f136c67d302501f8f7be071341df3d8429
Daniel Thalmann <daniel@thalmann.li>
2023-04-30 12:34:34 +0200

    add function getFriends

--- front/src/services/UserService.ts
+++ front/src/services/UserService.ts
@@ -1,4 +1,5 @@
 import {PUBLIC_API_URI} from "$env/static/public";
+import type { Friend } from "../types/friend.js";
 import type { User } from "../types/user.js";
 
 let userservice : UserService;
@@ -7,12 +8,12 @@ export class UserService
 {
     constructor ()
     {
-       
+
     }
 
     /**
      * return the status of login
-     * 
+     *
      * @returns Promise<boolean> true if the user is logged
      */
     async isLogged() : Promise<boolean>
@@ -48,6 +49,41 @@ export class UserService
 
         return await res.json();
     }
+
+    async getFriends() : Promise<Array<User>>
+    {
+        let users: Array<User> = new Array;
+
+        let res: Response = await fetch(`${PUBLIC_API_URI}/user/friend`, {
+            method: 'GET',
+            credentials: 'include'
+        });
+
+        const friends_list: Friend[] = await res.json();
+
+        for (const item of friends_list) {
+            let id = item.friend_id;
+            try {
+                if (item.accept_at == null)
+                    continue;
+                const res: Response = await fetch(`${PUBLIC_API_URI}/user/id/${id}`, {
+                    method: 'GET',
+                    credentials: 'include'
+                });
+                const new_friend: User = (await res.json());
+                users.push(new_friend);
+            }
+            catch (err)
+            {
+                console.error(err);
+            }
+        }
+
+        return Promise.resolve(users);
+
+    }
+
+
 
 }
 
