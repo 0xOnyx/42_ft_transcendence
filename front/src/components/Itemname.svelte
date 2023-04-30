2d8b3e31733ad9ee874f1c870eb78e89b14a286f
Daniel Thalmann <daniel@thalmann.li>
2023-04-30 09:50:15 +0200

    refactor image url

--- front/src/components/Itemname.svelte
+++ front/src/components/Itemname.svelte
@@ -6,6 +6,7 @@
     import {PUBLIC_API_URI} from "$env/static/public";
     import {goto} from "$app/navigation"
     import {Socket} from "socket.io-client";
+	import { imageUrl } from '../services/Utilities';
 
     //bg-green-600
     export let user : User;
@@ -56,13 +57,14 @@
 
         }
     }
+
 </script>
 
 <div on:click={getRoom} class="cursor-pointer rounded-xl bg-color5 p-5 flex items-center mt-1">
 
     <div class="mx-2 flex-shrink">
         <div class="w-[40px] h-[40px] bg-cover  rounded-full mx-auto"
-             style="background-image: url( /{user?.image_url || `image/default.png`} )">
+             style="background-image: url( {imageUrl(user?.image_url)} )">
         </div>
 
     </div>
