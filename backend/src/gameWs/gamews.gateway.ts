79903e0fbbe041ac11dae2527247e0e37f10384d
Daniel Thalmann <daniel@thalmann.li>
2023-05-06 01:30:41 +0200

    prepare socket game

--- backend/src/gameWs/gamews.gateway.ts
+++ backend/src/gameWs/gamews.gateway.ts
@@ -5,12 +5,13 @@ import {
     WebSocketServer,
     WsResponse,
   } from '@nestjs/websockets';
-  import { Injectable } from '@nestjs/common';
-  import { from, Observable } from 'rxjs';
-  import { map } from 'rxjs/operators';
-  import { Server } from 'socket.io';
-  import { OnEvent } from '@nestjs/event-emitter';
+import { Injectable } from '@nestjs/common';
+import { from, Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+import { OnEvent } from '@nestjs/event-emitter';
 import { Socket } from 'dgram';
+import { clearInterval } from 'timers';
 
   @Injectable()
   @WebSocketGateway({
@@ -20,21 +21,47 @@ import { Socket } from 'dgram';
   export class GameWsGateway {
     @WebSocketServer()
     server: Server;
+    interval: NodeJS.Timer;
+//     pong: Pong;
 
     async handleDisconnect(client: Socket) {
-      console.log("connection");
+      console.log("connection", Socket);
+
+      this.run();
+
     }
 
     @SubscribeMessage('events')
     findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-       return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+       
+      return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+
     }
     
     @OnEvent('game.create')
     @SubscribeMessage('events')
     gameCreate(payload: any) {
+
       this.server.emit('events', payload);
+
     }
+
+    run()
+    {
+      // this.pong = new Pong();
+
+      clearInterval(this.interval);
+      this.interval = setInterval(() => {
+        
+        this.server.emit('games', {
+          status : "READY",
+          time : new Date().getTime()
+        });
+
+      }, 200);  
+    }
+    
+  
 
   }
   
