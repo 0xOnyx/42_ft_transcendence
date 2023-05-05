f049e1b3f50cca8690220dba5e07e842ba53d8b9
Daniel Thalmann <daniel@thalmann.li>
2023-05-05 14:01:14 +0200

    add validation to nestjs pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,13 +4,14 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import sessionsMiddleware from './sessions'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(sessionsMiddleware)
   app.use(passport.initialize())
   app.use(passport.session())
-
+  app.useGlobalPipes(new ValidationPipe());
 
 
   const config = new DocumentBuilder()
