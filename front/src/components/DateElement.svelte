eade0d9c8067c5e23b052ab7f0a6ce448e602f91
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2023-05-17 15:32:38 +0200

    update date element

--- front/src/components/DateElement.svelte
+++ front/src/components/DateElement.svelte
@@ -2,7 +2,15 @@
     //bg-green-600
     export let created_at : Date;
 
+    function format2(value : string) : string
+    {
+        if(value.length < 2)
+            return '0' + value;
+        else
+            return value;
+    }
+
 </script>
 
-{created_at?.getDate()}.{created_at?.getMonth()}.{created_at?.getFullYear()}
-{created_at?.getHours()}:{created_at?.getMinutes()}
+{format2(created_at?.getDate().toString())}.{format2((created_at?.getMonth() + 1).toString())}.{created_at?.getFullYear()}
+{format2(created_at?.getHours().toString())}:{format2(created_at?.getMinutes().toString())}
