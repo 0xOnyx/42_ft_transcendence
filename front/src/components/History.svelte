3c00fc4a00823b21666a4fa10057821b29c5022e
Guillaume <guillaume@aubert.ws>
2023-05-17 15:22:46 +0000

    fixed bug

--- front/src/components/History.svelte
+++ front/src/components/History.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
 	import type { GameHistory, User } from "../types/user";
 	import DateElement from "./DateElement.svelte";
-	import UserInfo from "./UserInfo.svelte";
 	export let curUser : User;
-	export let gamehistory : GameHistory;
+	export let gamehistory : GameHistory[];
 </script>
 <table class="w-full">
-	
 	{#each gamehistory as g}
-	<tr>
-		<td>{g.player_one.name} vs {g.player_two.name}</td>
-		<td>{g.score_one} - {g.score_two}</td>
-		<td><DateElement created_at={new Date(g.created_at)}></DateElement> </td>
-		{#if (curUser.id == g.player_one_id && g.score_one > g.score_two) ||
-		     (curUser.id == g.player_two_id && g.score_one < g.score_two)}
-		<td>Win</td>
-		{:else}
-		<td>Lose</td>
-		{/if}
-	</tr>
+		<tr>
+			<td>{g.player_one.name} vs {g.player_two.name}</td>
+			<td>{g.score_one} - {g.score_two}</td>
+			<td><DateElement created_at={new Date(g.created_at)}></DateElement> </td>
+			{#if (curUser.id == g.player_one_id && g.score_one > g.score_two) ||
+				(curUser.id == g.player_two_id && g.score_one < g.score_two)}
+			<td>Win</td>
+			{:else}
+			<td>Lose</td>
+			{/if}
+		</tr>
 	{/each}
 </table>
