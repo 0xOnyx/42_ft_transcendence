85a8ab4ca06b87e7bfe12a46ddec142de01c8761
Guillaume <guillaume@aubert.ws>
2023-05-22 13:11:21 +0000

    draft

--- front/src/components/History.svelte
+++ front/src/components/History.svelte
@@ -4,17 +4,25 @@
 	export let curUser : User;
 	export let gamehistory : GameHistory[];
 </script>
-<table class="w-full">
-	{#each gamehistory as g}
+<table class="min-w-full text-center text-sm font-light sm:mt-8 md:max-2xl:mt-0">
+	<thead class="border-b font-medium dark:border-neutral-500">
 		<tr>
-			<td>{g.player_one.name} vs {g.player_two.name}</td>
-			<td>{g.score_one} - {g.score_two}</td>
-			<td><DateElement created_at={new Date(g.created_at)}></DateElement> </td>
+		  <th scope="col" class="px-6 py-4">Players</th>
+		  <th scope="col" class="px-6 py-4">Score</th>
+		  <th scope="col" class="px-6 py-4">Date</th>
+		  <th scope="col" class="px-6 py-4">Result</th>
+		</tr>
+	  </thead>
+	{#each gamehistory as g}
+		<tr class="border-b dark:border-neutral-500">
+			<td class=" px-6 py-2 font-medium">{g.player_one.name} vs {g.player_two.name}</td>
+			<td class="whitespace-nowrap px-6 py-2 font-medium">{g.score_one} - {g.score_two}</td>
+			<td class="whitespace-nowrap px-6 py-2 font-medium"><DateElement created_at={new Date(g.created_at)}></DateElement> </td>
 			{#if (curUser.id == g.player_one_id && g.score_one > g.score_two) ||
 				(curUser.id == g.player_two_id && g.score_one < g.score_two)}
-			<td>Win</td>
+			<td class="px-6 py-2 font-medium">Win</td>
 			{:else}
-			<td>Lose</td>
+			<td class="whitespace-nowrap px-6 py-2 font-medium">Lose</td>
 			{/if}
 		</tr>
 	{/each}
